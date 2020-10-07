7a0fd736ee780c86909ef1a2ee5999399c07595d
Divya H S <divya.s43@ad.infosys.com>
2020-10-07 19:53:27 +0530

    added source code

---
+++ Validator2/src/test/java/com/infy/BankLoanToTraineeApplicationTests.java
@@ -0,0 +1,34 @@
+//package com.infy;
+//
+//import org.junit.Rule;
+//import org.junit.rules.ExpectedException;
+//import org.junit.runner.RunWith;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import com.infy.dao.UserDAO;
+//import com.infy.service.UserService;
+//
+////DONT MODIFY NAME OF CLASS
+////DONT ADD/MODIFY/DELETE ANY METHOD
+////DONT DELETE/MODIFY INSTANCE VARIABLE(IF PRESENT)
+////DONT MODIFY ANNOTATIONS(IF PRESENT)
+//
+//@RunWith(SpringRunner.class)
+//public class BankLoanToTraineeApplicationTests {
+//
+//	@Rule
+//	ExpectedException expectedException = ExpectedException.none();
+//	
+//	UserDAO loanDAO;
+//	
+//	UserService loanService;
+//	
+//	public void sanctionLoanCustomerNotAvailableTest() throws Exception {
+//		
+//	}
+//	
+//	public void sanctionLoanLoanAlreadyTakenTest() throws Exception {
+//		
+//	}
+//
+//}
